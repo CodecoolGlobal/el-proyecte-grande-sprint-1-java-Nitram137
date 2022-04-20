e62d15df6131e174ebcddf9f6548c927b687a795
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-04-20 23:51:05 +0200

    FileController/ refactor createNewFolder method.

--- src/main/java/com/codecool/cloudheroes/controller/FileController.java
+++ src/main/java/com/codecool/cloudheroes/controller/FileController.java
@@ -4,6 +4,7 @@ import com.codecool.cloudheroes.message.ResponseMessage;
 import com.codecool.cloudheroes.model.FileModel;
 import com.codecool.cloudheroes.service.FilesStorageService;
 import com.google.gson.Gson;
+import org.apache.tomcat.util.http.fileupload.InvalidFileNameException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -36,21 +38,20 @@ public class FileController {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("OK", fileModels));
         } catch(FileNotFoundException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseMessage(
-                    "Path Doesn't exist", null));
+                    e.getMessage(), null));
         }
     }
 
     @PostMapping("/create")
-    public String createNewFolder(@RequestParam String newFolderName, @RequestParam String path) {
-        Map<String, String> response = new HashMap<>();
-        boolean newDirectory = new File(USER_DIRECTORY + path + newFolderName).mkdir();
-        File[] files = new File(USER_DIRECTORY + path).listFiles();
-        List<FileModel> fileModels = createFileModels(files);
-        Gson gson = new Gson();
-        response.put("content", gson.toJson(fileModels));
-        response.put("parentDirectory", path);
-        response.put("isCreated", String.valueOf(newDirectory));
-        return gson.toJson(response);
+    public ResponseEntity<ResponseMessage> createNewFolder(@RequestParam String folderName, @RequestParam String path) {
+        try {
+            storageService.createNewFolder(folderName, path);
+            List<FileModel> fileModels = storageService.getFolderContent(path);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("OK", fileModels));
+        } catch (FileNotFoundException | FileAlreadyExistsException | InvalidFileNameException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ResponseMessage(e.getMessage(), null));
+        }
     }
 
     @PutMapping("/rename")
