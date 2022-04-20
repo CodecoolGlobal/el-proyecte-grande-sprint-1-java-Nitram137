733396b101ab0d7a7043d9d9065a05678b9bd274
Nitram137 <nemethmartin99@gmail.com>
2022-04-20 12:14:11 +0200

    Rename main class

--- src/main/java/com/codecool/cloudheroes/CloudHeroesApp.java
+++ src/main/java/com/codecool/cloudheroes/CloudHeroesApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudHeroes {
+public class CloudHeroesApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudHeroes.class, args);
+        SpringApplication.run(CloudHeroesApp.class, args);
     }
 }
