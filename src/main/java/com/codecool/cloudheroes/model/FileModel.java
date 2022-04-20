8f3ca71eaaec0d5c48e3e3c01382b526b75a1a54
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-04-20 23:31:14 +0200

    FileModel/ add getters and setters for serialization.

--- src/main/java/com/codecool/cloudheroes/model/FileModel.java
+++ src/main/java/com/codecool/cloudheroes/model/FileModel.java
@@ -2,11 +2,27 @@ package com.codecool.cloudheroes.model;
 
 public class FileModel {
 
-    private final String name;
-    private final boolean isDirectory;
+    private String name;
+    private boolean isDirectory;
 
     public FileModel(String name, boolean isDirectory) {
         this.name = name;
         this.isDirectory = isDirectory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+
+    public void setDirectory(boolean directory) {
+        isDirectory = directory;
     }
 }
