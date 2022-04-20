2c6dc347f0bfc8ad574e851a8629156c4f1b4168
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-04-21 00:48:49 +0200

    FileController/ implement uploadFile method.

--- src/main/java/com/codecool/cloudheroes/controller/FileController.java
+++ src/main/java/com/codecool/cloudheroes/controller/FileController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.nio.file.FileAlreadyExistsException;
@@ -82,6 +83,19 @@ public class FileController {
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("OK", fileModels));
         } catch (FileNotFoundException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ResponseMessage(e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file,
+                                                      @RequestParam String path) {
+        try {
+            storageService.save(file, path);
+            List<FileModel> fileModels = storageService.getFolderContent(path);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("OK", fileModels));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(
                     new ResponseMessage(e.getMessage(), null));
         }
     }
