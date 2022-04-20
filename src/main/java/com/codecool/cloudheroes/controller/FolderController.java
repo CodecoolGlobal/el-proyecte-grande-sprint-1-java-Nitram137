1814b71afa509ee62d2caf411379831b14dfa1ff
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-04-20 19:54:32 +0200

    feat: FolderController/ implement getFOlderContent method.

--- src/main/java/com/codecool/cloudheroes/controller/FolderController.java
+++ src/main/java/com/codecool/cloudheroes/controller/FolderController.java
@@ -1,13 +1,13 @@
 package com.codecool.cloudheroes.controller;
 
 import com.codecool.cloudheroes.model.FileModel;
-
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -17,12 +17,18 @@ public class FolderController {
     private static final Path USER_DIRECTORY = Path.of(System.getProperty("user.dir") + "/doc-uploads");
 
     @GetMapping
-    public String getFolderContent(@RequestParam String path) throws IOException {
-        return "";
+    public String getFolderContent(@RequestParam String path) {
+        Map<String, String> response = new HashMap<>();
+        File[] files = new File(USER_DIRECTORY + path).listFiles();
+        List<FileModel> fileModels = createFileModels(files);
+        Gson gson = new Gson();
+        response.put("content", gson.toJson(fileModels));
+        response.put("parentDirectory", path);
+        return gson.toJson(response);
     }
 
     @PostMapping("/create")
-    public String createNewFolder(@RequestParam String newFolderName, @RequestParam String route) throws IOException {
+    public String createNewFolder(@RequestParam String newFolderName, @RequestParam String route) {
         return "";
     }
 
@@ -37,6 +43,10 @@ public class FolderController {
     }
 
     private List<FileModel> createFileModels(File[] files) {
-        return new ArrayList<>();
+        if (files == null) {
+            return new ArrayList<>();
+        }
+        return Arrays.stream(files).map(file ->  new FileModel(file.getName(), file.isDirectory())).
+                collect(Collectors.toList());
     }
 }
