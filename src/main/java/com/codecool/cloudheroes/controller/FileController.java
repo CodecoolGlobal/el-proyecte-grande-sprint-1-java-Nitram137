d25c7486daa7ee731052c6281c215da0f90f6594
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-04-21 00:30:26 +0200

    FileController/ refactor deleteFile method.

--- src/main/java/com/codecool/cloudheroes/controller/FileController.java
+++ src/main/java/com/codecool/cloudheroes/controller/FileController.java
@@ -3,26 +3,20 @@ package com.codecool.cloudheroes.controller;
 import com.codecool.cloudheroes.message.ResponseMessage;
 import com.codecool.cloudheroes.model.FileModel;
 import com.codecool.cloudheroes.service.FilesStorageService;
-import com.google.gson.Gson;
 import org.apache.tomcat.util.http.fileupload.InvalidFileNameException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Path;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
-
-    private static final Path USER_DIRECTORY = Path.of(System.getProperty("user.dir") + "/doc-uploads");
 
     FilesStorageService storageService;
 
@@ -81,23 +75,14 @@ public class FileController {
     }
 
     @DeleteMapping()
-    public String deleteFolder(@RequestParam String folderName, @RequestParam String path) {
-        Map<String, String> response = new HashMap<>();
-        boolean delete = new File(USER_DIRECTORY + path + folderName).delete();
-        File[] files = new File(USER_DIRECTORY + path).listFiles();
-        List<FileModel> fileModels = createFileModels(files);
-        Gson gson = new Gson();
-        response.put("content", gson.toJson(fileModels));
-        response.put("parentDirectory", path);
-        response.put("isDeleted", String.valueOf(delete));
-        return gson.toJson(response);
-    }
-
-    private List<FileModel> createFileModels(File[] files) {
-        if (files == null) {
-            return new ArrayList<>();
+    public ResponseEntity<ResponseMessage> deleteFile(@RequestParam String name, @RequestParam String path) {
+        try {
+            storageService.deleteFile(name, path);
+            List<FileModel> fileModels = storageService.getFolderContent(path);
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage("OK", fileModels));
+        } catch (FileNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new ResponseMessage(e.getMessage(), null));
         }
-        return Arrays.stream(files).map(file ->  new FileModel(file.getName(), file.isDirectory())).
-                collect(Collectors.toList());
     }
 }
