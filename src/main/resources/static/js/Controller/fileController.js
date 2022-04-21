5fdaa9ebfdcd1d04895072ec22908fe8222da768
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-04-21 13:11:43 +0200

    fileController.js/ convert to javascript class.

--- src/main/resources/static/js/Controller/fileController.js
+++ src/main/resources/static/js/Controller/fileController.js
@@ -1,60 +1,65 @@
 import {dataHandler} from "../DataHandler/dataHandler.js";
 
-export let fileController = {
-    currentRoute: null,
-    init: function () {
-        fileController.currentRoute = "/";
-    },
-    getFolderContent: function (folderName) {
-        dataHandler.getFolderContent(fileController.currentRoute + folderName + "/").then((response) => {
-            fileController.currentRoute = fileController.currentRoute + folderName + "/";
-            console.log(response);
-        }).catch((error) => {
-            alert(error);
-        });
-    },
-    moveBack: function () {
-        const routes = fileController.currentRoute.substring(1, fileController.currentRoute.length - 1).split("/");
+export default class FileController {
+
+    currentRoute;
+
+    constructor() {
+        this.currentRoute = "/";
+    }
+
+    async getFolderContent(folderName) {
+        this.currentRoute = this.currentRoute + folderName + "/";
+        return await dataHandler.getFolderContent(this.currentRoute);
+    }
+
+    moveBack() {
+        const routes = this.currentRoute.substring(1, this.currentRoute.length - 1).split("/");
         routes.pop();
         if (routes.length === 0) {
-            fileController.currentRoute = "/";
+            this.currentRoute = "/";
         } else {
-            fileController.currentRoute = "/" + routes.join("/") + "/";
+            this.currentRoute = "/" + routes.join("/") + "/";
         }
-        dataHandler.getFolderContent(fileController.currentRoute).then((response) => {
+        dataHandler.getFolderContent(this.currentRoute).then((response) => {
             console.log(response);
         })
-    },
-    createFolder: function (folderName) {
-        dataHandler.createFolder(folderName, fileController.currentRoute).then((response) => {
+    }
+
+    createFolder(folderName) {
+        dataHandler.createFolder(folderName, this.currentRoute).then((response) => {
             console.log(response);
         }).catch((error) => {
             alert(error);
         });
-    },
-    renameFolder: function (newName, oldName) {
-        dataHandler.renameFolder(newName, oldName, fileController.currentRoute).then((response) => {
+    }
+
+    renameFolder(newName, oldName) {
+        dataHandler.renameFolder(newName, oldName, this.currentRoute).then((response) => {
             console.log(response);
         }).catch((error) => {
             alert(error);
         });
-    },
-    moveFile: function (fileName, originalPath, newPath) {
+    }
+
+    moveFile(fileName, originalPath, newPath) {
         dataHandler.moveFile(fileName, originalPath, newPath).then((response) => {
             console.log(response);
         }).catch((error) => {
             alert(error);
         });
-    },
-    deleteFile: function(fileName, path) {
+    }
+
+    deleteFile(fileName, path) {
         dataHandler.deleteFile(fileName, path).then((response) => {
             console.log(response);
         }).catch((error) => {
             alert(error);
         });
-    },
-    uploadFile: function (file) {
-        dataHandler.uploadFile(file, fileController.currentRoute).then((response) => {
+    }
+
+    uploadFile(file) {
+        dataHandler.uploadFile(file, this.currentRoute).then((response) => {
             console.log(response);
         }).catch((error) => {
             alert(error);
