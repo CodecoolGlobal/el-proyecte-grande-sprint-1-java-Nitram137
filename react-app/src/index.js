f9ce8236a3edc4b12cd8fbf48c2796462ed679d4
Cseplonorbert <87705743+Cseplonorbert@users.noreply.github.com>
2022-05-16 11:48:02 +0200

    Inject Phaser Game component into App React component.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,19 +1,12 @@
-import Phaser from "phaser";
-import React from "react";
-import GameScene from "./GameScene";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const config = {
-    type: Phaser.AUTO,
-    width: 1680,
-    height: 950,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 800 }
-        }
-    },
-    scene: [GameScene]
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
-export default new Phaser.Game(config);
